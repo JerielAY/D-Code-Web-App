f76540c4b00846e08859bc64d7610c3cb8b4517a
austincraig2 <71351432+austincraig2@users.noreply.github.com>
2020-10-25 16:16:44 -0400

    Added new pages and set up nav bar
    Added projects, calendar, donate, and about us pages. You can access each with the navigation bar.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'D-CODE' });
 });
 
+// Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Calendar page
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', { title: 'Calendar' });
+});
+
+// About Us page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+// Donate page
+router.get('/donate', function(req, res, next) {
+  res.render('donate', { title: 'Donate' });
+});
+
 module.exports = router;
