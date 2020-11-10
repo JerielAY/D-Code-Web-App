1fc9102ddcdad5a229f4a7f8370a724399791669
BenPelczynski <71465979+BenPelczynski@users.noreply.github.com>
2020-11-09 22:37:47 -0500

    Added documentation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,12 @@
+// D-Code-Wep_App
+// Authors: Team 4
+// Date: 10/26/20
+// License: GPL 3.0
+// Instantiates and maintains user information with the MongoDB database.
+
 const { Schema, model } = require("mongoose");
+
+// Schema for information relating to each user.
 const userSchema = new Schema({
   username: {
     type: String,
@@ -11,7 +19,7 @@ const userSchema = new Schema({
   projects: { type: Schema.Types.ObjectId, ref: 'Projects' }
 });
 
-
+// Get user information from the login.
 userSchema.statics.findByLogin = async function(login) {
   let user = await this.findOne({
     username: login
@@ -24,6 +32,7 @@ userSchema.statics.findByLogin = async function(login) {
   return user;
 };
 
+// Instantiate user information.
 const User = model("User", userSchema, "users");
 
 module.exports = User;
