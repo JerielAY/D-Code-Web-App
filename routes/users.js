b77b012b278e1f87f29ddb9f1dd40be328fc94b5
BenPelczynski <71465979+BenPelczynski@users.noreply.github.com>
2020-11-10 12:44:43 -0500

    added documentation

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,11 @@
+
+// users.js
+// D-Code-Wep_App
+// Authors: Team 4
+// Date: 10/10/20
+// License: GPL 3.0
+// Connects the client to the appropriate user information upon login.
+
 var express = require('express');
 var router = express.Router();
 
